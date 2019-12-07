20d44325f3660f5dfd5059a1e64bfb8da8ef3dd4
Sanooj Pollayil Sadananadan <sanooj.pollayilsadananadan@cognizant.com>
2019-12-07 23:08:25 +0000

    typo fixes

--- arrays_and_strings/remove_duplicates_characters.py
+++ arrays_and_strings/remove_duplicates_characters.py
@@ -3,3 +3,20 @@
 # NOTE: One or two additional variables are fine. An extra copy of the array is not.
 # FOLLOW UP
 # Write the test cases for this method.
+
+def remove_duplicate_characters(word):
+    char_list = list(word)
+    current_duplicate_char = None    
+    for o_index,o_char in enumerate(char_list):
+        found_char = None 
+        for i_index,i_char in enumerate(char_list):
+            if o_char == i_char:
+                if found_char == None:
+                    found_char = o_char
+                else:
+                    char_list.pop(i_index)
+
+    print(char_list)
+
+
+remove_duplicate_characters("ssssstring")
